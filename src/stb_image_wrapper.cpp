cc3d9083146f800863226d136b20055262008cc7
Sebastian Hartte <sebastian@hartte.de>
2019-12-31 17:05:22 +0100

    Added bit flipping for PNGs.

--- src/stb_image_wrapper.cpp
+++ src/stb_image_wrapper.cpp
@@ -7,35 +7,37 @@
 #define STBI_ONLY_TGA
 #define STBI_ONLY_BMP
 #define STBI_ONLY_PNG
+
 #include <stb_image.h>
+#include <utility>
 
 NATIVE_API ApiBool Stb_BmpInfo(uint8_t *imageData, uint32_t imageDataSize,
                                int *width, int *height, ApiBool *hasAlpha) {
-  stbi__context ctx;
-  stbi__start_mem(&ctx, imageData, imageDataSize);
+    stbi__context ctx;
+    stbi__start_mem(&ctx, imageData, imageDataSize);
 
-  int comp;
-  if (stbi__bmp_info(&ctx, width, height, &comp)) {
-    *hasAlpha = (comp == 4);
-    return true;
-  }
+    int comp;
+    if (stbi__bmp_info(&ctx, width, height, &comp)) {
+        *hasAlpha = (comp == 4);
+        return true;
+    }
 
-  return false;
+    return false;
 }
 
 NATIVE_API void *Stb_BmpDecode(uint8_t *imageData, uint32_t imageDataSize,
                                int *width, int *height, ApiBool *hasAlpha,
                                uint32_t *pixelDataSize) {
-  stbi__context ctx;
-  stbi__start_mem(&ctx, imageData, imageDataSize);
+    stbi__context ctx;
+    stbi__start_mem(&ctx, imageData, imageDataSize);
 
-  int comp;
-  stbi__result_info ri;
-  auto data = stbi__bmp_load(&ctx, width, height, &comp, 4, &ri);
-  *hasAlpha = (comp == 4);
-  *pixelDataSize = (uint32_t)(*width * *height * 4);
+    int comp;
+    stbi__result_info ri;
+    auto data = stbi__bmp_load(&ctx, width, height, &comp, 4, &ri);
+    *hasAlpha = (comp == 4);
+    *pixelDataSize = (uint32_t) (*width * *height * 4);
 
-  return data;
+    return data;
 }
 
 // Frees memory returned by Stb_BmpDecode
@@ -44,31 +46,37 @@ NATIVE_API void Stb_BmpFree(void *data) { STBI_FREE(data); }
 //// PNG
 NATIVE_API ApiBool Stb_PngInfo(uint8_t *imageData, uint32_t imageDataSize,
                                int *width, int *height, ApiBool *hasAlpha) {
-  stbi__context ctx;
-  stbi__start_mem(&ctx, imageData, imageDataSize);
+    stbi__context ctx;
+    stbi__start_mem(&ctx, imageData, imageDataSize);
 
-  int comp;
-  if (stbi__png_info(&ctx, width, height, &comp)) {
-    *hasAlpha = (comp == 4);
-    return true;
-  }
+    int comp;
+    if (stbi__png_info(&ctx, width, height, &comp)) {
+        *hasAlpha = (comp == 4);
+        return true;
+    }
 
-  return false;
+    return false;
 }
 
 NATIVE_API void *Stb_PngDecode(uint8_t *imageData, uint32_t imageDataSize,
                                int *width, int *height, ApiBool *hasAlpha,
                                uint32_t *pixelDataSize) {
-  stbi__context ctx;
-  stbi__start_mem(&ctx, imageData, imageDataSize);
+    stbi__context ctx;
+    stbi__start_mem(&ctx, imageData, imageDataSize);
 
-  int comp;
-  stbi__result_info ri;
-  auto data = stbi__png_load(&ctx, width, height, &comp, 4, &ri);
-  *hasAlpha = (comp == 4);
-  *pixelDataSize = (uint32_t)(*width * *height * 4);
+    int comp;
+    stbi__result_info ri;
+    auto data = stbi__png_load(&ctx, width, height, &comp, 4, &ri);
+    *hasAlpha = (comp == 4);
+    *pixelDataSize = (uint32_t) (*width * *height * 4);
 
-  return data;
+    // STB decodes into RGBA, while we use BGRA internally, so let's flip
+    auto pixelData = reinterpret_cast<uint8_t *> (data);
+    for (auto i = 0u; i < *pixelDataSize; i += 4) {
+        std::swap(pixelData[i], pixelData[i + 2]);
+    }
+
+    return data;
 }
 
 // Frees memory returned by Stb_PngDecode
